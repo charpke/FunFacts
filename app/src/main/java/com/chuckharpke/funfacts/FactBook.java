b6640e6d490b4dc819fff149c8bcaf0e23960134
CHARLES HARPKE <Charpke@gmail.com>
2015-03-08 14:47:54 -0500

    initial commit

---
+++ app/src/main/java/com/chuckharpke/funfacts/FactBook.java
@@ -0,0 +1,43 @@
+package com.chuckharpke.funfacts;
+
+import java.util.Random;
+
+/**
+ * Created by charlesharpke on 8/26/14.
+ */
+public class FactBook {
+
+    // Member variable (properties about the object)
+    public String [] mFacts = {
+            "Ants stretch when they wake up in the morning.",
+            "Ostriches can run faster than horses.",
+            "Olympic gold medals are actually made mostly of silver",
+            "You are born with 300 bones; by the time you are an adult you will have 206.",
+            "It takes about 8 minutes for light from the Sun to reach Earth.",
+            "Some Bamboo plants can grow almost a meter in just one day.",
+            "The state of Florida is bigger than England",
+            "Some penguins can leap 2-3 meters out of the water.",
+            "On average, it takes 66 days to form a new habit.",
+            "Mammoths still walked the Earth when the Great Pyramid was being built.",
+            "Around 1495 Leonardo da Vinci sketched plans for a humanoid robot",
+            "By age 6, Mozart was writing his own compositions",
+            "Physicist Stephen Hawking's birthday, January 8, 1942 is the 300th anniversary of the death of Galileo",
+            "Einstein was a slow learner as a child and spoke very slowly"   };
+
+
+    // Method (abilities: things the object can do.)
+    public String getFact() {
+
+
+
+        String fact = "";
+
+        //Randomly select a fact
+        Random randomGenerator = new Random(); //Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mFacts.length);
+
+        fact = mFacts[randomNumber];
+
+        return fact;
+    }
+}
